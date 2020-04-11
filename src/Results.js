64195e6096b826c1002282113b96127110372274
Chris Conner <43318961+cconner57@users.noreply.github.com>
2020-04-10 21:09:45 -0700

    Add files via upload

--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Profile = styled.div`
+	background: #94e4fe;
+	border-radius: 50px;
+	box-shadow: 20px 20px 60px #7ec2d8, -20px -20px 60px #aaffff;
+	margin: 10px auto;
+	max-width: 200px;
+	padding: 25px;
+	text-align: center;
+
+	h1 {
+		text-decoration: underline;
+	}
+
+	p {
+		text-align: left;
+	}
+`;
 
 const Results = ({ data }) => {
 	return (
-		<div>
-			<h1>Info</h1>
-			<p>{data.Name}</p>
-			<p>{data.Email}</p>
-			<p>{data.City}</p>
-		</div>
+		<Profile>
+			<h1>Information</h1>
+			<p>Name: {data.Name}</p>
+			<p>Email: {data.Email}</p>
+			<p>City: {data.City}</p>
+		</Profile>
 	);
 };
 
